a1914979f171d965ea01c2d25d318bfa695bd8c1
hellomada <153454965+hellomada@users.noreply.github.com>
2025-08-17 11:37:35 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -6,7 +6,7 @@ export default function Dashboard() {
   const [body, setBody] = useState("");
 
   const sendEmail = async () => {
-    const res = await fetch("https://your-backend-url/send", {
+    const res = await fetch("http://localhost:8080", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
